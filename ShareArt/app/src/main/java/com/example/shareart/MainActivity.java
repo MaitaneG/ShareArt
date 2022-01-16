86c8e2027171ff950d448dc1915234d2564a70da
MaitaneG <Maitane>
2022-01-16 10:50:22 +0100

    Login hobetua

--- ShareArt/app/src/main/java/com/example/shareart/MainActivity.java
+++ ShareArt/app/src/main/java/com/example/shareart/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.shareart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView erregistroLinka;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        hasieratu();
     }
+
+    private void hasieratu() {
+        erregistroLinka = findViewById(R.id.erregistratu_linka);
+
+        erregistroLinka.setOnClickListener(this::erregistroraJoan);
+    }
+
+    private void erregistroraJoan(View view) {
+        Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+        startActivity(intent);
+    }
+
+
 }
