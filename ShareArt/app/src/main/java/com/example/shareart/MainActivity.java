84fd6d375268620743bf3e260599c841c5624afe
MaitaneG <Maitane>
2022-01-19 19:43:52 +0100

    Logina eta erregistroa

--- ShareArt/app/src/main/java/com/example/shareart/MainActivity.java
+++ ShareArt/app/src/main/java/com/example/shareart/MainActivity.java
@@ -18,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText pasahitzaEditText;
     private ImageButton hasiSaioBotoia;
 
+    /**
+     * Activity-a sortzen denean
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,30 +30,43 @@ public class MainActivity extends AppCompatActivity {
         hasieratu();
     }
 
+    /**
+     * Konponenteak hasieratzen dira
+     */
     private void hasieratu() {
+        // TextView
         erregistroLinka = findViewById(R.id.erregistratu_linka);
+        // EditText
         emailEditText = findViewById(R.id.textInputEditTextEmail);
         pasahitzaEditText = findViewById(R.id.textInputEditTextPasahitza);
+        // ImageButton
         hasiSaioBotoia = findViewById(R.id.ImageButtonHasiSaioa);
-
+        // OnClickListener
         erregistroLinka.setOnClickListener(this::erregistroraJoan);
         hasiSaioBotoia.setOnClickListener(this::hasiSaioa);
     }
 
+    /**
+     * Erregistratzeko pantailara doa
+     *
+     * @param view
+     */
     private void erregistroraJoan(View view) {
         Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
         startActivity(intent);
     }
 
+    /**
+     * Saioa hasteko
+     *
+     * @param view
+     */
     private void hasiSaioa(View view) {
         String email = emailEditText.getText().toString();
         String pasahitza = pasahitzaEditText.getText().toString();
 
-        if(email.trim().equals("")||pasahitza.trim().equals("")){
+        if (email.trim().equals("") || pasahitza.trim().equals("")) {
             Toast.makeText(MainActivity.this, "Gakoa guztiak bete behar dira", Toast.LENGTH_SHORT).show();
         }
-
     }
-
-
 }
--- ShareArt/app/src/main/java/com/example/shareart/RegisterActivity.java
+++ ShareArt/app/src/main/java/com/example/shareart/RegisterActivity.java
@@ -2,15 +2,29 @@ package com.example.shareart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private ImageButton atzeraBotoia;
+    private ImageButton erregistratuBotoia;
+    private TextInputEditText erabiltzaileaEditText;
+    private TextInputEditText emailEditText;
+    private TextInputEditText pasahitzaEditText;
+    private TextInputEditText pasahitzaBaieztatuEditText;
 
+    /**
+     * Activity-a sortzen denean
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +32,62 @@ public class RegisterActivity extends AppCompatActivity {
         hasieratu();
     }
 
+    /**
+     * Konponenteak hasieratzen dira
+     */
     private void hasieratu() {
-        atzeraBotoia= findViewById(R.id.atzera_botoia);
+        // Botoiak
+        atzeraBotoia = findViewById(R.id.atzera_botoia);
+        erregistratuBotoia = findViewById(R.id.ImageButtonErregistratu);
+        // EditText
+        erabiltzaileaEditText = findViewById(R.id.TextInputEditTextErabiltzaileaErregistratu);
+        emailEditText = findViewById(R.id.textInputEditTextEmail);
+        pasahitzaEditText = findViewById(R.id.TextInputEditTextPasahitzaErregistratu);
+        pasahitzaBaieztatuEditText = findViewById(R.id.TextInputEditTextBaieztatuPasahitzaErregistratu);
+        // OnClickListener
         atzeraBotoia.setOnClickListener(this::atzeraJoan);
+        erregistratuBotoia.setOnClickListener(this::erregistratu);
     }
 
+    /**
+     * Errgistratzen da
+     *
+     * @param view
+     */
+    private void erregistratu(View view) {
+        String erabiltzailea = erabiltzaileaEditText.getText().toString().trim();
+        String email = emailEditText.getText().toString().trim();
+        String pasahitza = pasahitzaEditText.getText().toString().trim();
+        String pasahitzaBaeieztatu = pasahitzaBaieztatuEditText.getText().toString().trim();
+
+        if (erabiltzailea.equals("") || email.equals("") || pasahitza.equals("") || pasahitzaBaeieztatu.equals("")) {
+            Toast.makeText(RegisterActivity.this, "Gako guztiak bete behar dira", Toast.LENGTH_SHORT);
+        } else if (!emailaKonprobatu(email)) {
+            Toast.makeText(RegisterActivity.this, "Emailaren formatua gaizki dago", Toast.LENGTH_SHORT).show();
+        } else {
+
+        }
+    }
+
+    /**
+     * Hasierako menura joateko
+     *
+     * @param view
+     */
     private void atzeraJoan(View view) {
         finish();
+    }
+
+    /**
+     * Email-a ondo idatzita dagoen konprobatzen du
+     *
+     * @param email
+     * @return
+     */
+    private boolean emailaKonprobatu(String email) {
+        String expression = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
