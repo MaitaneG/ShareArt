f147d6dda184cd00b5e41a8fa6a2b2dc826c402e
Gallastegui Juan <gallastegui.maitane@uni.lan>
2022-01-19 12:29:42 +0100

    Login eta register

--- ShareArt/app/src/main/java/com/example/shareart/MainActivity.java
+++ ShareArt/app/src/main/java/com/example/shareart/MainActivity.java
@@ -5,11 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView erregistroLinka;
+    private TextInputEditText emailEditText;
+    private TextInputEditText pasahitzaEditText;
+    private ImageButton hasiSaioBotoia;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,13 +28,27 @@ public class MainActivity extends AppCompatActivity {
 
     private void hasieratu() {
         erregistroLinka = findViewById(R.id.erregistratu_linka);
+        emailEditText = findViewById(R.id.textInputEditTextEmail);
+        pasahitzaEditText = findViewById(R.id.textInputEditTextPasahitza);
+        hasiSaioBotoia = findViewById(R.id.ImageButtonHasiSaioa);
 
         erregistroLinka.setOnClickListener(this::erregistroraJoan);
+        hasiSaioBotoia.setOnClickListener(this::hasiSaioa);
     }
 
     private void erregistroraJoan(View view) {
-        Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
         startActivity(intent);
+    }
+
+    private void hasiSaioa(View view) {
+        String email = emailEditText.getText().toString();
+        String pasahitza = pasahitzaEditText.getText().toString();
+
+        if(email.trim().equals("")||pasahitza.trim().equals("")){
+            Toast.makeText(MainActivity.this, "Gakoa guztiak bete behar dira", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 
--- ShareArt/app/src/main/java/com/example/shareart/RegisterActivity.java
+++ ShareArt/app/src/main/java/com/example/shareart/RegisterActivity.java
@@ -2,13 +2,28 @@ package com.example.shareart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ImageButton atzeraBotoia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        hasieratu();
+    }
+
+    private void hasieratu() {
+        atzeraBotoia= findViewById(R.id.atzera_botoia);
+        atzeraBotoia.setOnClickListener(this::atzeraJoan);
+    }
+
+    private void atzeraJoan(View view) {
+        finish();
     }
 }
