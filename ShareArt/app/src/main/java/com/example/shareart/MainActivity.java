1453a3dc5302a8c070e7736f7d22079b7c2a80e4
MaitaneG <Maitane>
2022-01-20 14:17:24 +0100

    Registro hecho, login con correo y contraseña hecho, solo falta poner el SHA-256 al proyecto de firebase y seguir el video

--- ShareArt/app/src/main/java/com/example/shareart/MainActivity.java
+++ ShareArt/app/src/main/java/com/example/shareart/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.shareart;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +15,19 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,9 +35,13 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText emailEditText;
     private TextInputEditText pasahitzaEditText;
     private ImageButton hasiSaioBotoia;
+    private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient mGoogleSignInClient;
+    private SignInButton hasiSaioaGooglekinBotoia;
 
     /**
      * Activity-a sortzen denean
+     *
      * @param savedInstanceState
      */
     @Override
@@ -41,9 +63,25 @@ public class MainActivity extends AppCompatActivity {
         pasahitzaEditText = findViewById(R.id.textInputEditTextPasahitza);
         // ImageButton
         hasiSaioBotoia = findViewById(R.id.ImageButtonHasiSaioa);
+        hasiSaioaGooglekinBotoia = findViewById(R.id.ButtonHasiSaioaGoogle);
+        //FireBase
+        firebaseAuth = FirebaseAuth.getInstance();
+        // Login Google-ekin
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("701358991762-doqqgkfo09u14ki3vteqovgb2j7lvl7b.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(MainActivity.this, googleSignInOptions);
+
         // OnClickListener
         erregistroLinka.setOnClickListener(this::erregistroraJoan);
-        hasiSaioBotoia.setOnClickListener(this::hasiSaioa);
+        hasiSaioBotoia.setOnClickListener(this::hasiSaioaKorreoaEtaPasahitzarekin);
+        hasiSaioaGooglekinBotoia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resultLauncher.launch(new Intent(mGoogleSignInClient.getSignInIntent()));
+            }
+        });
     }
 
     /**
@@ -61,12 +99,61 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param view
      */
-    private void hasiSaioa(View view) {
-        String email = emailEditText.getText().toString();
-        String pasahitza = pasahitzaEditText.getText().toString();
+    private void hasiSaioaKorreoaEtaPasahitzarekin(View view) {
+        String email = emailEditText.getText().toString().trim();
+        String pasahitza = pasahitzaEditText.getText().toString().trim();
 
-        if (email.trim().equals("") || pasahitza.trim().equals("")) {
+        if (email.isEmpty() || pasahitza.isEmpty()) {
             Toast.makeText(MainActivity.this, "Gakoa guztiak bete behar dira", Toast.LENGTH_SHORT).show();
+        } else {
+            firebaseAuth.signInWithEmailAndPassword(email, pasahitza).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Emaila edo pasahitza gaizki daude", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
+    }
+
+
+    private ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                Intent intent = mGoogleSignInClient.getSignInIntent();
+
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(intent);
+
+                try {
+                    GoogleSignInAccount account =task.getResult(ApiException.class);
+
+                    assert account!=null;
+
+                }catch (ApiException e){
+
+                }
+            }
+        }
+    });
+
+    private void firebaseAuthWithGoogle(String idToken){
+        AuthCredential credential= GoogleAuthProvider.getCredential(idToken,null);
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(MainActivity.this,HomeActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(MainActivity.this, "Sesioa ondo hasi da", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Arazo bat egon da sesioa hastean", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- ShareArt/app/src/main/java/com/example/shareart/RegisterActivity.java
+++ ShareArt/app/src/main/java/com/example/shareart/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.shareart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,8 +8,15 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,9 +28,12 @@ public class RegisterActivity extends AppCompatActivity {
     private TextInputEditText emailEditText;
     private TextInputEditText pasahitzaEditText;
     private TextInputEditText pasahitzaBaieztatuEditText;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
 
     /**
      * Activity-a sortzen denean
+     *
      * @param savedInstanceState
      */
     @Override
@@ -41,9 +52,12 @@ public class RegisterActivity extends AppCompatActivity {
         erregistratuBotoia = findViewById(R.id.ImageButtonErregistratu);
         // EditText
         erabiltzaileaEditText = findViewById(R.id.TextInputEditTextErabiltzaileaErregistratu);
-        emailEditText = findViewById(R.id.textInputEditTextEmail);
+        emailEditText = findViewById(R.id.TextInputEditTextEmailErregistratu);
         pasahitzaEditText = findViewById(R.id.TextInputEditTextPasahitzaErregistratu);
         pasahitzaBaieztatuEditText = findViewById(R.id.TextInputEditTextBaieztatuPasahitzaErregistratu);
+        // FireBase
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
         // OnClickListener
         atzeraBotoia.setOnClickListener(this::atzeraJoan);
         erregistratuBotoia.setOnClickListener(this::erregistratu);
@@ -60,13 +74,48 @@ public class RegisterActivity extends AppCompatActivity {
         String pasahitza = pasahitzaEditText.getText().toString().trim();
         String pasahitzaBaeieztatu = pasahitzaBaieztatuEditText.getText().toString().trim();
 
-        if (erabiltzailea.equals("") || email.equals("") || pasahitza.equals("") || pasahitzaBaeieztatu.equals("")) {
-            Toast.makeText(RegisterActivity.this, "Gako guztiak bete behar dira", Toast.LENGTH_SHORT);
+        if (erabiltzailea.isEmpty() || email.isEmpty() || pasahitza.isEmpty() || pasahitzaBaeieztatu.isEmpty()) {
+            Toast.makeText(RegisterActivity.this, "Gako guztiak bete behar dira.", Toast.LENGTH_SHORT);
         } else if (!emailaKonprobatu(email)) {
-            Toast.makeText(RegisterActivity.this, "Emailaren formatua gaizki dago", Toast.LENGTH_SHORT).show();
+            Toast.makeText(RegisterActivity.this, "Emailaren formatua gaizki dago.", Toast.LENGTH_SHORT).show();
         } else {
-
+            if (!pasahitza.equals(pasahitzaBaeieztatu)) {
+                Toast.makeText(RegisterActivity.this, "Pasahitzak ez dira berdinak.", Toast.LENGTH_SHORT).show();
+            } else {
+                if (pasahitza.length() < 8) {
+                    Toast.makeText(RegisterActivity.this, "Pasahitza oso motza da. Gutxienez 8 karaketere izan behar ditu.", Toast.LENGTH_SHORT).show();
+                } else {
+                    sortuErabiltzailea(erabiltzailea, email, pasahitza);
+                }
+            }
         }
+    }
+
+    /**
+     * Erabiltzailea sortzen du FireBase Authentification-en eta Firebaseko datubasean
+     *
+     * @param erabiltzailea
+     * @param email
+     * @param pasahitza
+     */
+    private void sortuErabiltzailea(String erabiltzailea, String email, String pasahitza) {
+        firebaseAuth.createUserWithEmailAndPassword(email, pasahitza).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    String id = firebaseAuth.getCurrentUser().getUid();
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("erabiltzailea", erabiltzailea);
+                    map.put("email", email);
+                    Toast.makeText(RegisterActivity.this, "Erabiltzailea ondo sortu da", Toast.LENGTH_SHORT).show();
+                    firebaseFirestore.collection("Users").document(id).set(map);
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Arazo bat egon da, erabiltzailea sortzean", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        finish();
     }
 
     /**
