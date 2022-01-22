bb219634e4ed5db2450ef3cd65d5def6f3d366b3
MaitaneG <Maitane>
2022-01-22 23:56:20 +0100

    argazkia igo botoia eta pantaila

--- ShareArt/app/src/main/java/com/example/shareart/Fragments/HomeFragment.java
+++ ShareArt/app/src/main/java/com/example/shareart/Fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.shareart.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,8 +10,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.shareart.R;
+import com.example.shareart.activities.PostActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class HomeFragment extends Fragment {
+
+    private View view;
+    private FloatingActionButton floatingActionButton;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -20,6 +26,15 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        floatingActionButton=view.findViewById(R.id.floatingButtonArgitalpen);
+        floatingActionButton.setOnClickListener(this::argitaratuArgazkiBat);
+        return view;
+    }
+
+    private void argitaratuArgazkiBat(View view) {
+        Intent intent = new Intent(getContext(), PostActivity.class);
+        startActivity(intent);
+
     }
 }
