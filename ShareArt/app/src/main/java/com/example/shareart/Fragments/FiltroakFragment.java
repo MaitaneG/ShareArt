e08d4dfaddd6e50af6488ddbf6c153f5832667b3
MaitaneG <Maitane>
2022-01-22 17:17:38 +0100

    Fragmentuak, floating button

---
+++ ShareArt/app/src/main/java/com/example/shareart/Fragments/FiltroakFragment.java
@@ -0,0 +1,25 @@
+package com.example.shareart.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.shareart.R;
+
+public class FiltroakFragment extends Fragment {
+
+    public FiltroakFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_filtroak, container, false);
+    }
+}
--- ShareArt/app/src/main/java/com/example/shareart/activities/HomeActivity.java
+++ ShareArt/app/src/main/java/com/example/shareart/activities/HomeActivity.java
@@ -1,16 +1,67 @@
 package com.example.shareart.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.shareart.Fragments.FiltroakFragment;
+import com.example.shareart.Fragments.HomeFragment;
+import com.example.shareart.Fragments.PerfilaFragment;
 import com.example.shareart.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        hasieratu();
+    }
+
+    private void hasieratu() {
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+        bottomNavigation.setOnItemSelectedListener(this::nabigatu);
+        // Fragmentu lehenetsia
+        openFragment(new HomeFragment());
+    }
+
+    private boolean nabigatu(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            // Home fragmentua
+            case R.id.navigation_home:
+                openFragment(new HomeFragment());
+                return true;
+            // Filtroak fragmentua
+            case R.id.navigation_filtroak:
+                openFragment(new FiltroakFragment());
+                return true;
+            // Perfila fragmentua
+            case R.id.navigation_perfila:
+                openFragment(new PerfilaFragment());
+                return true;
+        }
+        return false;
+    }
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finishAffinity();
     }
 }
