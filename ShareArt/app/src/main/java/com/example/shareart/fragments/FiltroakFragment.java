88e090434446fc9ad28367897debed0c512ddd9a
MaitaneG <Maitane>
2022-01-31 18:20:25 +0100

    Pantalla filtroak, zure perfila diseinua hasi

--- ShareArt/app/src/main/java/com/example/shareart/fragments/FiltroakFragment.java
+++ ShareArt/app/src/main/java/com/example/shareart/fragments/FiltroakFragment.java
@@ -1,16 +1,29 @@
 package com.example.shareart.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.shareart.R;
+import com.example.shareart.activities.MainActivity;
+import com.example.shareart.providers.AuthProvider;
 
 public class FiltroakFragment extends Fragment {
+
+    private View view;
+    private Toolbar toolbar;
+    private AuthProvider authProvider;
 
     public FiltroakFragment() {
         // Required empty public constructor
@@ -20,6 +33,36 @@ public class FiltroakFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_filtroak, container, false);
+        view = inflater.inflate(R.layout.fragment_filtroak, container, false);
+        // Tresna-barra
+        toolbar = (Toolbar) view.findViewById(R.id.ToolBar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        setHasOptionsMenu(true);
+        // Sesioak kudeatzeko
+        authProvider = new AuthProvider();
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.main_menu,menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            saioaItxi();
+            return true;
+        }
+        return false;
+    }
+
+    private void saioaItxi() {
+        authProvider.saioaItxi();
+
+        Intent intent = new Intent(getContext(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
